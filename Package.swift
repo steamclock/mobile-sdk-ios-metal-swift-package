cd2fc34ba4b2e5b5e72bb95cc916cbde8ca4d291
mtehver <mark.tehver@gmail.com>
2022-02-22 10:11:15 +0200

    Version 4.4.5-rc.1

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CartoMobileSDK",
-            url: "https://nutifront.s3.amazonaws.com/sdk_snapshots/sdk4-ios-metal-4.4.4.zip",
-            checksum: "901a3a730a2e05bdcaa33b6d46859d1d9509ed095c38a9515b1c6b8a11b835d4"
+            url: "https://nutifront.s3.amazonaws.com/sdk_snapshots/sdk4-ios-metal-4.4.5-rc.1.zip",
+            checksum: "a42b3ef4d7452eb9d06678f3c7eedbbc982a5c00e42f7e72eae54dbc2f622821"
         )
     ]
 )
