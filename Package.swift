f3b86ca47135fba0ef1a681e8edc300c7033b127
mauriceschenk <64644062+mauriceschenk@users.noreply.github.com>
2024-04-24 15:06:28 -0700

    removed checksum

--- Package.swift
+++ Package.swift
@@ -18,8 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CartoMobileSDK",
-            url: "https://steamclock-public.s3.ca-central-1.amazonaws.com/carto-sdk/carto-sdk-ios-metal-4.4.7-rc1.zip",
-            checksum: "fec807235bbfe907e6a56d7e2e547c87aa83c03c1938420bdf95c348c7d109e2"
+            url: "https://steamclock-public.s3.ca-central-1.amazonaws.com/carto-sdk/carto-sdk-ios-metal-4.4.7-rc1.zip"
         )
     ]
 )
