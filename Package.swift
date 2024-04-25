63e28a94ab15c24e6345002e5abf8ce27356dc24
mauriceschenk <64644062+mauriceschenk@users.noreply.github.com>
2024-04-24 17:20:54 -0700

    added missing characters to checksum

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "CartoMobileSDK",
             url: "https://steamclock-public.s3.ca-central-1.amazonaws.com/carto-sdk/carto-sdk-ios-metal-4.4.7-rc1.zip",
-            checksum: "f1c16381edf9102a9de7454dacc7634baab345384f02d229f31a1187528bb2"
+            checksum: "f1c16381edf9102a9de74e54dacc7634baab345384f02d229f31ea1187528bb2"
         )
     ]
 )
